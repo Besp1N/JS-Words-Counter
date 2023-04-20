f0082182b859aeeb49190de68c2a499087a0b1e2
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2023-04-20 23:33:35 +0200

    added JS logic

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
 const textArea = document.querySelector(".text-area");
+let onChange = () => {
+    const data = textArea.value;
+    const charLength = data.length;
+    const wordsNum = data.split(" ").length;
+    const sentensesNum = data.split(".").length;
+
+    document.getElementById("num-characters").innerHTML = charLength;
+    document.getElementById("words-length").innerHTML = wordsNum;
+    document.getElementById("sentences-count").innerHTML =sentensesNum;
+}
+textArea.addEventListener("input",onChange);
